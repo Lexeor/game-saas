c735297b78a1d890707ff48aa2fe35c5bec5d6c9
Alexander Tarasov <lexeor.ross@gmail.com>
2026-03-19 14:43:11 +0100

    add toaster

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { HashRouter } from 'react-router-dom';
 import { GoogleOAuthProvider } from '@react-oauth/google';
+import { Toaster } from 'sonner';
 import { OverlayScrollbars } from 'overlayscrollbars';
 import 'overlayscrollbars/overlayscrollbars.css';
 import './index.css';
@@ -24,6 +25,19 @@ createRoot(document.getElementById('root')!).render(
       <HashRouter>
         <AuthProvider>
           <App />
+          <Toaster
+            position="top-center"
+            toastOptions={{
+              style: {
+                background: 'rgba(12, 18, 32, 0.85)',
+                backdropFilter: 'blur(16px)',
+                border: '0',
+                color: '#dde6ff',
+                borderRadius: '14px',
+                fontSize: '13px',
+              },
+            }}
+          />
         </AuthProvider>
       </HashRouter>
     </GoogleOAuthProvider>
